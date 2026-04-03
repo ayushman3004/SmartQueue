770160dd58743b5e8595adcf61df4b39be41d555
Ayushman Bhattacharya <ayushman.rick007@gmail.com>
2026-04-03 14:57:31 +0530

    fixed issues

--- src/api/otp.api.js
+++ src/api/otp.api.js
@@ -1,9 +1,9 @@
 import axios from "./axios";
 
 export const sendOtp = async (phoneNumber) => {
-  return await axios.post("/api/otp/send", { phoneNumber });
+  return await axios.post("/otp/send", { phoneNumber });
 };
 
 export const verifyOtp = async (phoneNumber, otp) => {
-  return await axios.post("/api/otp/verify", { phoneNumber, otp });
+  return await axios.post("/otp/verify", { phoneNumber, otp });
 };
